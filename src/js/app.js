0ed1d9579e136434b03aa8f556d6995ba05d06ab
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2021-05-05 02:23:07 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
 import NicknameForm from './NicknameForm';
 import OnlineChatWidget from './OnlineChatWidget';
 
-const url = 'alex-m18-ahj-sse-chat.herokuapp.com';
 
 const form = new NicknameForm();
 form.bindToDOM(document.body);
 form.init();
 
-const chat = new OnlineChatWidget(url, form);
+const chat = new OnlineChatWidget('http://localhost:7070/', form);
 chat.bindToDOM(document.querySelector('#online_chat_widget_container'));
 chat.init();
